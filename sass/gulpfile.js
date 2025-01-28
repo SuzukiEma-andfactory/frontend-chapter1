2703b264d08ef74daaf409051edf3b2d64071a53
SuzukiEma-andfactory <suzuki_ema@andfactory.co.jp>
2025-01-28 13:53:28 +0900

    gulpをインストール

---
+++ sass/gulpfile.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+'use strict'
+
+var { src, watch, dest, series } = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+
+const sassBuild = (done) => {
+  src('./_src/sass/**/*.scss')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(dest('./css'));
+  done();
+};
+
+const watchScss = () => {
+  watch('./_src/sass/**/*.scss', sassBuild);
+};
+
+exports.default = series(sassBuild, watchScss);
+exports.build = sassBuild;

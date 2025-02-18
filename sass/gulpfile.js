424c7c5033b0526c1e34f62f3ca8e313db1ad710
SuzukiEma-andfactory <suzuki_ema@andfactory.co.jp>
2025-02-18 14:29:02 +0900

    セクション52

--- sass/gulpfile.js
+++ sass/gulpfile.js
@@ -1,13 +1,16 @@
 'use strict';
 
-const { src, watch, dest, series } = require('gulp');
+const { src, watch, dest, series, parallel } = require('gulp');
 const gulpSass = require('gulp-sass');
 const sass = require('sass');
 const sassLint = require('gulp-sass-lint');
 const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const path = require('path');
 
 const compileSass = gulpSass(sass);
 
+// Sassをコンパイルしてブラウザをリロード
 const sassBuild = (done) => {
   src('./_src/sass/**/*.scss')
     .pipe(sourcemaps.init())
@@ -20,13 +23,33 @@ const sassBuild = (done) => {
     .pipe(sassLint.failOnError())
     .pipe(compileSass({ outputStyle: 'expanded' }))
     .pipe(sourcemaps.write('.')) // ソースマップをCSSファイルと同じディレクトリに出力
-    .pipe(dest('./css'));
+    .pipe(dest('./css'))
+    .pipe(browserSync.stream()); // xSassをコンパイル後にブラウザを更新
   done();
 };
 
-const watchScss = () => {
-  watch('./_src/sass/**/*.scss', sassBuild);
+// `browser-sync` を設定してブラウザを自動で開く
+const serve = (done) => {
+  browserSync.init({
+    server: {
+      baseDir: path.resolve(__dirname, '_src/sass/section10'),
+      // css/section10/sass/style.cssにアクセス
+      routes: {
+        '/css': path.resolve(__dirname, 'css'),
+      },
+    },
+    open: true, // ブラウザを自動で開く
+    notify: false, // リロード時の通知をオフにする
+  });
+  done();
 };
 
-exports.default = series(sassBuild, watchScss);
+// ファイル変更を監視してリロード
+const watchFiles = () => {
+  watch('./_src/sass/**/*.scss', sassBuild); // SCSS変更時にコンパイル
+  watch('./*.html').on('change', browserSync.reload); // HTML変更時にリロード
+  watch('./css/**/*.css').on('change', browserSync.reload); // CSS変更時にリロード
+};
+
+exports.default = series(sassBuild, parallel(serve, watchFiles));
 exports.build = sassBuild;
